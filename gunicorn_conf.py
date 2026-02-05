b9a29941f5a606fc55a19b0e340a94452f9199c8
Mixty <138919084+KittyGirlthing@users.noreply.github.com>
2026-02-05 11:08:30 -0800

    Create/Migrate DB on prod start

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,5 @@
+from polycules import migrate
+
 # Gunicorn config variables, based on https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask/
 loglevel = "info"
 errorlog = "-"  # stderr
@@ -7,3 +9,8 @@ graceful_timeout = 120
 timeout = 120
 keepalive = 5
 threads = 3
+preload_app = True
+
+def on_starting(server):
+    """Executes code before the master process is initialized"""
+    migrate()
